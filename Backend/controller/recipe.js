817ea1c58a3d5ac7cdd117209d84afbabd202774
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-08-04 14:27:16 +0100

    a

--- Backend/controller/recipe.js
+++ Backend/controller/recipe.js
@@ -1,11 +1,24 @@
+const Recipes = require("../models/recipe")
+
 const getRecipes=(req,res)=>{
-    res.json({message:"Hello"})
+    const recipes=await Recipes.find()
+    return res.json(recipes)
 }
 const getRecipe=(req,res)=>{
     res.json({message:"Hello"})
 }
-const addRecipe=(req,res)=>{
-    res.json({message:"Hello"})
+const addRecipe=async(req,res)=>{
+    const{tittle,ingredients,instructions,time}=req.body; 
+    
+    if(!tittle || !ingredients || !instructions ) {
+        res.json({message:"required fields can not be empty habibo"})
+    }
+
+    const newRecipe = await Recipes.create({
+        tittle,ingredients,ingredients,time
+    })
+
+    return res.json(newRecipe)
 }
 const editRecipe=(req,res)=>{
     res.json({message:"Hello"})
