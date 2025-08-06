caf7222c17b5c5d7180b3222d8fe96b537be5fd6
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-08-06 15:39:50 +0100

    commit changes

---
+++ Frontend/food-blog-app/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+
+const Home = () => {
+    return (
+        <>
+            <section className="home">
+                <div className="left">
+                    <h1>Food recipe</h1>
+                    <h5>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</h5>
+                    <button>sahre your Recipe</button>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Home
