d35363588e50be081e5e1714b4be2c6653ef7738
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-08-09 22:54:49 +0100

    commit changes

--- Frontend/food-blog-app/src/pages/Home.jsx
+++ Frontend/food-blog-app/src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
                 <div className="left">
                     <h1>Food recipe</h1>
                     <h5>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</h5>
-                    <button>sahre your Recipe</button>
+                    <button>share your Recipe</button>
                 </div>
                 <div className="right">
                     <img src={Foodrecipe} alt="foodrecipe img" width="320px" height="300px" />
