26d5cc5af6e6fdcea2ad2a0aa46a34378aa7902d
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-08-09 23:27:15 +0100

    cmmit changes

--- Frontend/food-blog-app/src/components/Navbar.jsx
+++ Frontend/food-blog-app/src/components/Navbar.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+
+
 
 export default function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <>
+            <header>
+                <h2>Food Blog</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>My Recipe</li>
+                    <li>Favorites</li>
+                    <li>Login</li>
+                </ul>
+            </header>
+        </>
+    )
 }
