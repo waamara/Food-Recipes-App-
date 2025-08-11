d2d942994332a077c5929bbbb5be891c6354c8b5
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-08-11 12:22:46 +0100

    commit

--- Frontend/food-blog-app/src/App.jsx
+++ Frontend/food-blog-app/src/App.jsx
@@ -2,15 +2,19 @@ import './App.css'
 import {createBrowserRouter,RouterProvider} from "react-router-dom"
 import Home from './pages/Home'
 import MainNavigation from './components/MainNavigation'
+import axios from 'axios'
 
 const getAllRecipes = async () => {
   let allRecipes=[]
-  await
+  await axios.get('http://localhost:5000/recipe').then(res=>{
+    allRecipes=res.data
+  })
+  return allRecipes
 }
 
 const router = createBrowserRouter([
   {path:"/",element:<MainNavigation/>,children:[
-    {path:"/",element:<Home/>}
+    {path:"/",element:<Home/>,loader:getAllRecipes}
   ]},
 ])
 
