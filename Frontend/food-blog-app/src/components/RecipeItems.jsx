c30f97f5b844b42b7f5068c9567fe7577b9c6670
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-08-12 16:00:11 +0100

    commit

--- Frontend/food-blog-app/src/components/RecipeItems.jsx
+++ Frontend/food-blog-app/src/components/RecipeItems.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
 import { useLoaderData } from 'react-router-dom'
+import foodImg from '../assets/foodRecipe.png'
 
 export default function RecipeItems() {
-const allRecipes=useLoaderData()
-console.logall(allRecipes)
+    const allRecipes = useLoaderData()
+    console.logall(allRecipes)
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className='card-container'>
+                {
+                    allRecipes?.map((item,index)=>{
+                        return(
+                            <div key={index} className='card'> 
+                                <img src={foodImg} alt="food image" />
+                                <div className='card-body'>
+                                    <div className='title'>
+                                        {item.title}
+                                    </div>
+                                    <div className='icons'>
+
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+
     )
 }
