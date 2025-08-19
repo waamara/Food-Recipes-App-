8763b8153e035ad24edb6fcfbd7db893323ec619
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-08-19 16:11:51 +0100

    commt

--- Backend/controller/user.js
+++ Backend/controller/user.js
@@ -1,5 +1,14 @@
-const userSignUp=async(res,req)=>{
+const User = require("../models/user")
 
+const userSignUp=async(res,req)=>{
+    const {email,password}=req.body
+    if(!email || !password){
+        return res.status(400).json({message:"email and password are required"})
+    }
+    let user=await User.findOne({email})
+    if(user){
+        return res.status(400).json({error:"Email is already Exist"})
+    }
 } 
 
 const userLogin=async(res,req)=>{
