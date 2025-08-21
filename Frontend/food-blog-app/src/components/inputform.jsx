3e0dc9a3bd6eec754b217b50cdf886ed2f35a7d7
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-08-21 18:35:25 +0100

    commit

--- Frontend/food-blog-app/src/components/inputform.jsx
+++ Frontend/food-blog-app/src/components/inputform.jsx
@@ -1,19 +1,26 @@
+import { useState } from "react"
 
 
 const inputform = () => {
+    const handleOnSubmit = (e) => {
+        const [email,setEmail] = useState("")
+        const [password,setPassword]=useState("")
+        const [isSignUp,setIsSignUp]=useState(false)
+        e.preventDefault()
+    }
     return (
         <>
-            <form className="form">
+            <form className="form" onSubmit={handleOnSubmit}>
                 <div className="form-control">
                     <label >Email</label>
-                    <input type="email" className="input" required />
+                    <input type="email" className="input" onChange={(e)=>setEmail(e.target.value)} required />
                 </div>
                 <div className="form-control">
                     <label>Password</label>
-                    <input type="text" name="" id="" />
+                    <input type="password" className="input" onChange={(e)=>setPassword(e.target.value)} required />
                 </div>
-                <button type="submit">Login</button>
-                <p>Create new account</p>
+                <button type="submit">{(isSignUp)?"Sign Up" : "Login"}Login</button><br />
+                <p onClick={()=>setIsSignUp(true)}>{(isSignUp) ? "Already have an Account" : "Create New Account"}</p>
             </form>
         </>        
     )
