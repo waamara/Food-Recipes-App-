cdc4ea48df7e5637c76a420e06079cbaaecdd7d6
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-08-21 23:20:52 +0100

    check login page

--- Frontend/food-blog-app/src/components/Navbar.jsx
+++ Frontend/food-blog-app/src/components/Navbar.jsx
@@ -1,18 +1,26 @@
 
+import { useState } from "react"
+import { NavLink } from "react-router-dom"
+
 
 
 export default function Navbar() {
+    const [isOpen,setIsOpen]=useState(false)
+    const checkLogin=()=>{
+        setIsOpen(false)
+    }
     return (
         <>
             <header>
                 <h2>Food Blog</h2>
                 <ul>
-                    <li>Home</li>
-                    <li>My Recipe</li>
-                    <li>Favorites</li>
-                    <li>Login</li>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/myRecipe">Myrecipe</NavLink></li>
+                    <li><NavLink to="/favRecipe">Favorites</NavLink></li>
+                    <li onClick={checkLogin}><p className="login">Login</p></li>
                 </ul>
             </header>
+            {(isOpen) && <Modal onClose={() => setIsOpen(false)}> <InputForm setIsOpen={()=>setIsOpen(false)}/></Modal>}
         </>
     )
 }
