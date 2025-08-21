2d7ba80037dd11903627659004bfecfa0a7fdd66
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-08-21 12:33:48 +0100

    comm

--- Frontend/food-blog-app/src/components/inputform.jsx
+++ Frontend/food-blog-app/src/components/inputform.jsx
@@ -3,6 +3,18 @@
 const inputform = () => {
     return (
         <>
+            <form className="form">
+                <div className="form-control">
+                    <label >Email</label>
+                    <input type="email" className="input" required />
+                </div>
+                <div className="form-control">
+                    <label>Password</label>
+                    <input type="text" name="" id="" />
+                </div>
+                <button type="submit">Login</button>
+                <p>Create new account</p>
+            </form>
         </>        
     )
 }
