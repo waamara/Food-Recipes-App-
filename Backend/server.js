016f6bcff054eac4a3d39f1023b42b6b7394ca71
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-08-23 22:49:27 +0100

    c

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,16 @@
 const express=require("express") 
 const app=express() 
 const dotenv=require("dotenv").config()
-
+const connectDB=require("./config/connectionDb")
 const PORT=process.env.PORT || 3000
+const cors =require("cors")
+app.use(cors())
+app.use(expess.static("public"))
 
-
+app.use("/",require("./routes/user"))
 app.use("/recipe",require("./routes/recipe"))
-
+connectDB()
+app.use(express.json())
 app.listen(PORT,(err)=>{
     console.log(`app is listening on port ${PORT}`)
 })
