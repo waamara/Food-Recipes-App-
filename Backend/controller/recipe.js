8ff23d61fd3f59fc6c66c7ea5e8c3c3c7cbf7f47
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-08-23 22:29:58 +0100

    added pablic

--- Backend/controller/recipe.js
+++ Backend/controller/recipe.js
@@ -1,5 +1,17 @@
 const recipe = require("../models/recipe")
-const Recipes = require("../models/recipe")
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp/my-uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+
+const upload = multer({ storage: storage })
 
 const getRecipes = (req, res) => {
     const recipes = await Recipes.find()
