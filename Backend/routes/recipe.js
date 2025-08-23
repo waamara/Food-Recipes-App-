f4811882dba80d8b19e239375257131ef8b48c70
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-08-23 22:36:33 +0100

    commit

--- Backend/routes/recipe.js
+++ Backend/routes/recipe.js
@@ -1,10 +1,10 @@
 const express=require("express")
-const { getRecipes,getRecipe,addRecipe,deleteRecipe,editRecipe} = require("../controller/recipe")
+const { getRecipes,getRecipe,addRecipe,deleteRecipe,editRecipe,upload} = require("../controller/recipe")
 const router=express.Router()
 
 router.get("/",getRecipes) //Get All recipies 
 router.get("/:id",getRecipe) 
-router.post("/",addRecipe)  
+router.post("/",upload.single('file'),addRecipe)  
 routet.put("/:id",editRecipe) 
 router.delete("/:id",deleteRecipe)
 
